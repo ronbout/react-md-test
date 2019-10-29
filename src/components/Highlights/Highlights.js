33b95e54cb49cce0d8944a6a8342aa623a8c9f0d
Ron Boutilier <ronbout@yahoo.com>
2019-10-29 15:06:15 -0500

    Converted the highlight column to an editable field.  Still has Edit in the kebab menu until we decide which is preferable.

--- src/components/Highlights/Highlights.js
+++ src/components/Highlights/Highlights.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	DataTable,
 	TableHeader,
 	TableBody,
 	TableRow,
-	TableColumn
+	TableColumn,
+	EditDialogColumn
 } from "react-md";
 import "./css/highlights.css";
-import highlights from "./highlightsData";
+import highlightsData from "./highlightsData";
 
 //import "./styles.css";
 import KebabMenu from "./KebabMenu";
@@ -16,53 +17,75 @@ const onMenuClick = (action, ndx) => {
 	alert(action);
 };
 
-const Highlights = () => (
-	<section className="highlights-section">
-		<DataTable baseId="menu-table">
-			<TableHeader>
-				<TableRow>
-					<TableColumn></TableColumn>
-					<TableColumn grow>Highlight</TableColumn>
-					<TableColumn
-						tooltipLabel="# of Skills attached"
-						tooltipDelay={500}
-						tooltipPosition="left"
-					>
-						Skills
-					</TableColumn>
-				</TableRow>
-			</TableHeader>
-			<TableBody>
-				{highlights.map(({ highlight, sequence, skills }, i) => {
-					let skillsTooltip = {};
-					if (skills.length) {
-						skillsTooltip = {
-							tooltipStyle: { background: "#ddd", color: "black" },
-							tooltipPosition: "left",
-							tooltipLabel: skills.map(s => (
-								<p>
-									{s.id}-{s.name}
-								</p>
-							))
-						};
-					}
+const Highlights = () => {
+	const [highlights, setHighlights] = useState(highlightsData);
 
-					return (
-						<TableRow key={`hrow-${sequence}`.replace(/\s+/g, "-")}>
-							<TableColumn>{i + 1}</TableColumn>
-							<TableColumn>{highlight}</TableColumn>
-							<TableColumn
-								className={skills.length ? "" : "md-text--error"}
-								{...skillsTooltip}
-							>
-								{skills.length ? skills.length : "--"}
-							</TableColumn>
-							<KebabMenu ndx={i} onMenuClick={onMenuClick} />
-						</TableRow>
-					);
-				})}
-			</TableBody>
-		</DataTable>
-	</section>
-);
+	const onHighlightChange = (ndx, highlight) => {
+		const newHighlights = highlights.slice();
+		newHighlights[ndx].highlight = highlight;
+		setHighlights(newHighlights);
+	};
+
+	return (
+		<section className="highlights-section">
+			<DataTable baseId="menu-table">
+				<TableHeader>
+					<TableRow>
+						<TableColumn></TableColumn>
+						<TableColumn grow>Highlight</TableColumn>
+						<TableColumn
+							tooltipLabel="# of Skills attached"
+							tooltipDelay={500}
+							tooltipPosition="left"
+						>
+							Skills
+						</TableColumn>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{highlights.map(({ highlight, sequence, skills }, i) => {
+						let skillsTooltip = {};
+						if (skills.length) {
+							skillsTooltip = {
+								tooltipStyle: { background: "#ddd", color: "black" },
+								tooltipPosition: "left",
+								tooltipLabel: skills.map(s => (
+									<p>
+										{s.id}-{s.name}
+									</p>
+								))
+							};
+						}
+
+						return (
+							<TableRow key={`hrow-${sequence}`.replace(/\s+/g, "-")}>
+								<TableColumn>{i + 1}</TableColumn>
+								<EditDialogColumn
+									large
+									className="table-edit-full-width"
+									dialogStyle={{ width: "550px" }}
+									style={{ width: "100%" }}
+									value={highlight}
+									onChange={val => onHighlightChange(i, val)}
+									label="Edit Highlight"
+									rows={1}
+									maxRows={4}
+									maxlength={200}
+									helpText="max chars = 200"
+								/>
+								<TableColumn
+									className={skills.length ? "" : "md-text--error"}
+									{...skillsTooltip}
+								>
+									{skills.length ? skills.length : "--"}
+								</TableColumn>
+								<KebabMenu ndx={i} onMenuClick={onMenuClick} />
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</DataTable>
+		</section>
+	);
+};
 export default Highlights;
