4b93c000fd88185438b4a075e7435dee4d930693
Ron Boutilier <ronbout@yahoo.com>
2019-12-18 20:13:01 -0600

    Developed and tested an autocomplete field to read in company names from the api

--- src/components/testAutoComplete/AutocompleteCompany.js
+++ src/components/testAutoComplete/AutocompleteCompany.js
@@ -1,10 +1,59 @@
 /* AutocompleteCompany.js */
-import React from "react";
+import React, { useState } from "react";
+import { Autocomplete } from "react-md";
+import sampleData from "./sampleData";
+import dataFetch from "assets/js/dataFetch";
+
+const API_COMPANY_SEARCH = "companies/search";
 
 const AutocompleteCompany = props => {
+	const [company, setCompany] = useState("");
+	const [results, setResults] = useState([]);
+	const [autoResults, setAutoResults] = useState([]);
+
+	window.apiUrl = "http://localhost/3sixd/api/";
+
+	const handleSearch = async searchVal => {
+		const apiQuery = `&name=${searchVal}`;
+
+		let results = await dataFetch(API_COMPANY_SEARCH, apiQuery);
+		if (results.error) {
+			console.log("Fetch error: ", results);
+		} else {
+			const data = results.map(r => {
+				return r.name;
+			});
+			setResults(results ? results : []);
+			setAutoResults(data ? data : []);
+		}
+	};
+	const handleChange = (val, e) => {
+		setCompany(val);
+		handleSearch(val);
+	};
+
+	const handleSelect = (val, valNdx, matches) => {
+		setCompany(val);
+		console.log(results[valNdx]);
+	};
+
 	return (
 		<div>
-			<h1>Company Autocomplete</h1>
+			<div>
+				<Autocomplete
+					id="company"
+					label="Company Name"
+					placeholder="Abc Towing"
+					data={autoResults}
+					value={company}
+					onChange={handleChange}
+					onAutocomplete={handleSelect}
+					filter={null}
+				/>
+			</div>
+			<div>
+				<h3>Company: {company ? company : "None selected"}</h3>
+			</div>
 		</div>
 	);
 };
--- src/components/testAutoComplete/TestAutoComplete.js
+++ src/components/testAutoComplete/TestAutoComplete.js
@@ -1,14 +1,11 @@
 /* TestAutoComplete.js */
 import React from "react";
-import MenuAutocomplete from "./MenuAutoComplete";
+// import MenuAutocomplete from "./MenuAutoComplete";
 import AutocompleteCompany from "./AutocompleteCompany";
 
 const TestAutoComplete = props => {
 	return (
 		<div>
-			<h1>test auto complete</h1>
-			<h2>Menu Auto Complete</h2>
-			<MenuAutocomplete />
 			<h2>Autocomplete of company name</h2>
 			<AutocompleteCompany />
 		</div>
